ee46ca867f8f5092abd091c44ad937df98f19ef9
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-19 06:49:23 +0000

    Remove the error message

--- monty_funcs_3.c
+++ monty_funcs_3.c
@@ -40,15 +40,9 @@ void pstr(stack_t **stack, unsigned int line_number)
 {
 	stack_t *tmp = (*stack)->next;
 
-	while (tmp && tmp->n)
+	while (tmp && tmp->n && (tmp->n > 0 && tmp->n <= 127))
 	{
-		if (tmp->n < 0 || tmp->n > 127)
-		{
-			empty_stack(line_number, "can't pchar, value out of range");
-			return;
-		}
-		else
-			printf("%c", tmp->n);
+		printf("%c", tmp->n);
 		tmp = tmp->next;
 	}
 	printf("\n");
