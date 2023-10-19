dc7c683a1b1b9948fea81d4903c020d0ef3e7e3d
Mohammed Ahmed <ma2286572@gmail.com>
2023-10-19 02:10:10 +0000

    Implement the push, pall and pint opcodes

---
+++ monty_funcs.c
@@ -0,0 +1,86 @@
+#include "monty.h"
+
+/**
+ * push - Push an element to the stack or queue
+ * @stack: The stack
+ * @line_number: This is used for printing error messages
+ *
+ * Return: Void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	stack_t *new, *tmp;
+
+	if (op_tokens[1] == NULL)
+	{
+		no_number_found(line_number);
+		return;
+	}
+	if (!is_atoi())
+	{
+		no_number_found(line_number);
+		return;
+	}
+	new = malloc(sizeof(stack_t));
+	if (!new)
+	{
+		(malloc_error());
+		return;
+	}
+	new->n = atoi(op_tokens[1]);
+	if ((*stack)->n == STACK)
+	{
+		tmp = (*stack)->next;
+		new->next = tmp;
+		new->prev = *stack;
+		if (tmp)
+			(tmp)->prev = new;
+		(*stack)->next = new;
+	}
+	else
+	{
+		tmp = *stack;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+		new->next = NULL;
+		new->prev = tmp;
+	}
+}
+
+
+/**
+ * pall - Print all the elements of the stack
+ * @stack: A pointer to the head of the stack
+ * @line_number: This member is used for ptinting error messages
+ *
+ * Return: void
+ */
+void pall(stack_t **stack, __attribute__((unused))unsigned int line_number)
+{
+	stack_t *tmp = (*stack)->next;
+
+	while (tmp)
+	{
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+}
+
+/**
+ * pint - Print the elements at the top of the stack
+ * @stack: A pointer to the head of the stack
+ * @line_number: This member is used for ptinting error messages
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	int *exit_status = get_exit_status();
+
+	if ((*stack)->next == NULL)
+		*exit_status = cannot_pint(line_number);
+	else
+		printf("%d\n", (*stack)->next->n);
+
+}
